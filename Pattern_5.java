2d9e9bc23bfbf4f4fac2c2d3d184156fd8b52d1e
Dinesh sahoo <80614494+freedinesh88@users.noreply.github.com>
2023-07-17 18:58:00 +0530

    Add files via upload

---
+++ Pattern_5.java
@@ -0,0 +1,23 @@
+class Pattern_5{
+    public static void main(String[] args){
+        int space=0, star=4;
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=space;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=star;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            space++;
+            star--;
+        }
+    }
+}
+
+
+
+// ****
+//  ***
+//   **
+//    *
